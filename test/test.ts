57097c30d98a5f39a45e93c0a1336936a395338d
Hyeon Kim <simnalamburt@gmail.com>
2020-08-19 00:19:39 +0900

    Remove AVA with Mocha

---
+++ test/test.ts
@@ -0,0 +1,7 @@
+import assert from 'assert'
+
+import josa from '..'
+
+it('placeholder', () => {
+  assert.equal(josa(), 'Hello, world!')
+})
--- tests/test.ts
+++
@@ -1,6 +0,0 @@
-import test from 'ava'
-import josa from '..'
-
-test('placeholder', (t) => {
-  t.is(josa(), 'Hello, world!')
-})
