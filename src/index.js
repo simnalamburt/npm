4ac686acd23ee6a2e8811f1cb09609c517898020
Hyeon Kim <simnalamburt@gmail.com>
2020-08-19 01:48:27 +0900

    Run 'yarn fmt'

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-'use strict';
+'use strict'
 
-const through = require('through2');
-const slm = require('slm');
-const PluginError = require('plugin-error');
-const replaceExt = require('replace-ext');
+const through = require('through2')
+const slm = require('slm')
+const PluginError = require('plugin-error')
+const replaceExt = require('replace-ext')
 
 module.exports = (opts = {}) =>
   through.obj((file, enc, cb) => {
-    opts.filename = file.path;
+    opts.filename = file.path
 
     if (file.data) {
-      opts.data = file.data;
+      opts.data = file.data
     }
 
     file.path = replaceExt(
       file.path,
-      opts.extension !== undefined ? opts.extension : '.html',
-    );
+      opts.extension !== undefined ? opts.extension : '.html'
+    )
 
     if (file.isStream()) {
-      return cb(new PluginError('gulp-slm', 'Streaming not supported'));
+      return cb(new PluginError('gulp-slm', 'Streaming not supported'))
     }
 
     if (file.isBuffer()) {
       try {
-        const contents = String(file.contents);
-        const compiled = slm.compile(contents, opts)(opts.locals || opts.data);
-        file.contents = Buffer.from(compiled);
+        const contents = String(file.contents)
+        const compiled = slm.compile(contents, opts)(opts.locals || opts.data)
+        file.contents = Buffer.from(compiled)
       } catch (e) {
-        return cb(new PluginError('gulp-slm', e));
+        return cb(new PluginError('gulp-slm', e))
       }
     }
 
-    cb(null, file);
-  });
+    cb(null, file)
+  })
--- test/test.js
+++ test/test.js
@@ -1,22 +1,22 @@
-'use strict';
+'use strict'
 
-const slm = require('../');
-const compile = require('slm').compile;
+const slm = require('../')
+const compile = require('slm').compile
 
-const through = require('through2');
-const path = require('path');
-const fs = require('fs');
-const extname = require('path').extname;
-const gulp = require('gulp');
-const File = require('vinyl');
-const PluginError = require('plugin-error');
+const through = require('through2')
+const path = require('path')
+const fs = require('fs')
+const extname = require('path').extname
+const gulp = require('gulp')
+const File = require('vinyl')
+const PluginError = require('plugin-error')
 
-const assert = require('assert');
+const assert = require('assert')
 
 //
 // Absolute path of fixtures/helloworld.slm
 //
-const filename = path.join(__dirname, 'fixtures', 'helloworld.slm');
+const filename = path.join(__dirname, 'fixtures', 'helloworld.slm')
 
 //
 // Mockup plugin to set the file.data property
@@ -24,14 +24,14 @@ const filename = path.join(__dirname, 'fixtures', 'helloworld.slm');
 //   from file.data
 //
 function setData() {
-  return through.obj(function(file, enc, cb) {
+  return through.obj(function (file, enc, cb) {
     file.data = {
       title: 'Greetings!',
-    };
-    this.push(file);
+    }
+    this.push(file)
 
-    return cb();
-  });
+    return cb()
+  })
 }
 
 //
@@ -39,20 +39,20 @@ function setData() {
 //   result with require('slm').compile
 //
 function expectStream(options) {
-  options = options || {};
+  options = options || {}
 
-  options.filename = filename;
-  const compiled = compile(fs.readFileSync(filename, 'UTF-8'), options);
-  const expected = compiled(options.data || options.locals);
-  const ext = '.html';
+  options.filename = filename
+  const compiled = compile(fs.readFileSync(filename, 'UTF-8'), options)
+  const expected = compiled(options.data || options.locals)
+  const ext = '.html'
 
   return through.obj((file, enc, cb) => {
-    assert.equal(expected, String(file.contents));
-    assert.equal(extname(file.path), ext);
-    assert.equal(extname(file.relative), file.relative ? ext : '');
+    assert.equal(expected, String(file.contents))
+    assert.equal(extname(file.path), ext)
+    assert.equal(extname(file.relative), file.relative ? ext : '')
 
-    return cb();
-  });
+    return cb()
+  })
 }
 
 //
@@ -60,11 +60,8 @@ function expectStream(options) {
 //
 describe('gulp-slm', () => {
   it('should compile my slm files into HTML', () => {
-    gulp
-      .src(filename)
-      .pipe(slm())
-      .pipe(expectStream());
-  });
+    gulp.src(filename).pipe(slm()).pipe(expectStream())
+  })
 
   it('should compile my slm files into HTML with locals passed in', () => {
     gulp
@@ -74,16 +71,16 @@ describe('gulp-slm', () => {
           locals: {
             title: 'Yellow Curled',
           },
-        }),
+        })
       )
       .pipe(
         expectStream({
           locals: {
             title: 'Yellow Curled',
           },
-        }),
-      );
-  });
+        })
+      )
+  })
 
   it('should compile my slm files into HTML with data passed in', () => {
     gulp
@@ -93,16 +90,16 @@ describe('gulp-slm', () => {
           data: {
             title: 'Yellow Curled',
           },
-        }),
+        })
       )
       .pipe(
         expectStream({
           data: {
             title: 'Yellow Curled',
           },
-        }),
-      );
-  });
+        })
+      )
+  })
 
   it('should compile my slm files into HTML with data property', () => {
     gulp
@@ -114,9 +111,9 @@ describe('gulp-slm', () => {
           data: {
             title: 'Greetings!',
           },
-        }),
-      );
-  });
+        })
+      )
+  })
 
   it('should always return contents as a buffer', () => {
     gulp
@@ -124,26 +121,26 @@ describe('gulp-slm', () => {
       .pipe(slm())
       .pipe(
         through.obj((file, enc, cb) => {
-          assert(file.contents instanceof Buffer);
-          return cb();
-        }),
-      );
-  });
+          assert(file.contents instanceof Buffer)
+          return cb()
+        })
+      )
+  })
 
   it('should throw an error if given contents is a stream', () => {
-    const stream = slm();
+    const stream = slm()
 
-    stream.on('error', err => {
-      assert(err instanceof PluginError);
-    });
+    stream.on('error', (err) => {
+      assert(err instanceof PluginError)
+    })
     stream.write(
       new File({
         path: filename,
         base: path.join(__dirname, 'fixtures'),
         cwd: __dirname,
         contents: fs.createReadStream(filename),
-      }),
-    );
-    stream.end();
-  });
-});
+      })
+    )
+    stream.end()
+  })
+})
