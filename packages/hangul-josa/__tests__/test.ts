628aaad853fd9475cdeacda4d643e336af2f7c75
Hyeon Kim <simnalamburt@gmail.com>
2020-08-22 04:29:54 +0900

    hangul-josa: Basic implementation

--- packages/hangul-josa/__tests__/test.ts
+++ packages/hangul-josa/__tests__/test.ts
@@ -1,7 +1,44 @@
 import assert from 'assert'
 
-import josa from '..'
+import { josa, 은, 는, 을, 를, 와, 과, 이, 가 } from '..'
 
-it('placeholder', () => {
-  assert.equal(josa(), 'Hello, world!')
+it('basic tests', () => {
+  const player = '지현'
+  const skill = '구르기'
+  assert.equal(
+    josa`${player}${는} ${skill}${을} 사용했다!`,
+    '지현은 구르기를 사용했다!'
+  )
+
+  const fruit = '파인애플'
+  const apple = '사과'
+  assert.equal(
+    josa`${fruit}${와} ${apple}${이} 잘 썰려있다.`,
+    '파인애플과 사과가 잘 썰려있다.'
+  )
+
+  const dr_kim = '김 박사'
+  const paper = '논문'
+  assert.equal(
+    josa`${dr_kim}${은} ${paper}${를} 제출했다.`,
+    '김 박사는 논문을 제출했다.'
+  )
+
+  const christian = '기독교도'
+  const buddhist = '불교인'
+  assert.equal(
+    josa`${christian}${과} ${buddhist}${가} 화합하는 사회`,
+    '기독교도와 불교인이 화합하는 사회'
+  )
+})
+
+it('ignore null input', () => {
+  assert.equal(josa`Hello, ${undefined}world!`, 'Hello, world!')
+  assert.equal(josa`Hello, ${null}world!`, 'Hello, world!')
+})
+
+it('empty input', () => {
+  assert.equal(josa``, '')
+  assert.equal(josa`${undefined}`, '')
+  assert.equal(josa`${null}`, '')
 })
--- packages/hangul-josa/src/index.ts
+++ packages/hangul-josa/src/index.ts
@@ -1,4 +1,47 @@
-export default function josa(): string {
-  // TODO: Implement something
-  return 'Hello, world!'
+export const 은_는 = Symbol()
+export { 은_는 as 은, 은_는 as 는 }
+
+export const 을_를 = Symbol()
+export { 을_를 as 을, 을_를 as 를 }
+
+export const 이_가 = Symbol()
+export { 이_가 as 이, 이_가 as 가 }
+
+export const 와_과 = Symbol()
+export { 와_과 as 와, 와_과 as 과 }
+
+export function josa(tpl: TemplateStringsArray, ...keys: any[]): string {
+  let buf = ''
+
+  for (let i = 0; i < keys.length; ++i) {
+    buf += tpl[i]
+
+    const key = keys[i]
+    if (key == null) {
+      continue
+    }
+
+    // This code can handle zero-length stringn with NaN propagation
+    const haveJong = (buf.charCodeAt(buf.length - 1) - 0xac00) % 28 > 0
+    switch (key) {
+      case 은_는:
+        buf += haveJong ? '은' : '는'
+        break
+      case 을_를:
+        buf += haveJong ? '을' : '를'
+        break
+      case 이_가:
+        buf += haveJong ? '이' : '가'
+        break
+      case 와_과:
+        buf += haveJong ? '과' : '와'
+        break
+      default:
+        // In this position, key's type is "All types except null or undefined",
+        // which makes safe to call `key.toString()`.
+        buf += key.toString()
+    }
+  }
+
+  return buf + tpl[keys.length]
 }
