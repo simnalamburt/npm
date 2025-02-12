6415e009d1e409b14a9510aa5a63fcf66ce8a15c
Hyeon Kim <simnalamburt@gmail.com>
2025-02-12 17:20:11 +0900

    packages/gulp-slm: Replace microbundle with tsdown

---
+++ packages/gulp-slm/tsdown.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, type Config } from 'tsdown'
+
+const config: Config = defineConfig({
+  entry: 'src/index.ts',
+  clean: true,
+  minify: true,
+  sourcemap: true,
+  dts: true,
+  format: ['cjs', 'esm'],
+  outputOptions(options, format) {
+    switch (format) {
+      case 'cjs':
+        delete options.dir
+        options.file = 'dist/gulp-slm.js'
+        break
+      case 'es':
+        delete options.dir
+        options.file = 'dist/gulp-slm.modern.js' // For backward compatibility
+        break
+    }
+    return options
+  },
+})
+
+export default config
