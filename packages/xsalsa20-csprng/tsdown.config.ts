a8a9800c79504d2d9ee048d74cd3ff8ea0210db0
Jihyeon Kim <simnalamburt@gmail.com>
2026-02-16 18:04:08 +0900

    (misc) Remove default tsdown configs

--- packages/xsalsa20-csprng/tsdown.config.ts
+++ packages/xsalsa20-csprng/tsdown.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig, type UserConfig } from 'tsdown'
 
 const config: UserConfig = defineConfig({
-  entry: 'src/index.ts',
-  clean: true,
   minify: true,
   sourcemap: true,
-  dts: {
-    sourcemap: false,
-  },
   format: ['es', 'umd'],
   outputOptions: {
     name: 'xsalsa20Csprng',
